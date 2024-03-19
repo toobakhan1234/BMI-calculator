5c0934f6230804e6598bb5aa8bfb1f1dfc4ec2ec
Tooba Khan <157229325+toobakhan1234@users.noreply.github.com>
2024-03-19 22:07:11 +0500

    Add files via upload

---
+++ homework/main.js
@@ -0,0 +1,5 @@
+var weighInKg = 70;
+var heightInMeters = 1.75;
+var bmi = weighInKg / (heightInMeters * heightInMeters);
+console.log("your BMI is ".concat(bmi));
+
---
+++ homework/main.ts
@@ -0,0 +1,6 @@
+let weighInKg =70;
+let heightInMeters =1.75;
+let bmi = weighInKg / (heightInMeters * heightInMeters);
+console.log(`your BMI is ${bmi}`);
+
+
